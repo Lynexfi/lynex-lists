f6a835342d01c4d1901f986d35419033cf8654cc
efeDaniels <polycrystalefe@gmail.com>
2024-10-02 09:55:59 +0300

    chore: add blacklisting feature & test blacklisting zero address

--- src/updateGauges.cjs
+++ src/updateGauges.cjs
@@ -1,22 +1,28 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 async function main() {
-  const fetch = (await import('node-fetch')).default;
-
-  const res = await fetch('https://api.lynex.fi/api/v1/fusions');
+  const fetch = (await import("node-fetch")).default;
+  const blacklistedGauges = [
+    "0x0000000000000000000000000000000000000000",
+    "0x41d245492B3a9C5a5364e8F6Bb75035abfe4079e", //vAMM-MECH/WETH
+    "0xC8E3E29e006Cd48a4f0AD0A06558E41853a1bAbD", // EURO3/USD+ (Stable)
+  ];
+  const res = await fetch("https://api.lynex.fi/api/v1/fusions");
   const json = await res.json();
-  let result = json.data.map((item) => {
-    return {
-      address: item.address,
-      symbol: item.symbol,
-      gauge: item.gauge.address,
-      token0: item.token0.address,
-      token1: item.token1.address,
-      isStrategy: item.isGamma,
-      bribe: item.gauge.bribe,
-    };
-  });
+  let result = json.data
+    .filter((item) => !blacklistedGauges.includes(item.gauge.address))
+    .map((item) => {
+      return {
+        address: item.address,
+        symbol: item.symbol,
+        gauge: item.gauge.address,
+        token0: item.token0.address,
+        token1: item.token1.address,
+        isStrategy: item.isGamma,
+        bribe: item.gauge.bribe,
+      };
+    });
   result = result.sort(function (a, b) {
     if (a.address < b.address) {
       return -1;
@@ -26,7 +32,7 @@ async function main() {
     }
     return 0;
   });
-  const destination = path.resolve('./config', 'gauges.json');
+  const destination = path.resolve("./config", "gauges.json");
   fs.writeFileSync(destination, JSON.stringify(result, null, 2));
 }
 
