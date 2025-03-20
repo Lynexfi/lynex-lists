ed0bb89ed04a0c36b34f5be4dd192b1fd35bcee3
Garrett Lee <garr.c.lee@gmail.com>
2025-03-20 11:54:56 -0500

    adjust promoted

--- src/constants/strategies.ts
+++ src/constants/strategies.ts
@@ -449,6 +449,7 @@ export const strategies: Strategies = {
         address: "0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f",
       },
       percentage: 70,
+      promoted: true,
     },
     {
       symbol: "USDC/LYNX",
@@ -621,7 +622,6 @@ export const strategies: Strategies = {
         address: "0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f",
       },
       percentage: 70,
-      promoted: true,
     },
     {
       symbol: "aBTC/wBTC",
