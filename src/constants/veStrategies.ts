066ee433211b5aadf53b01721d636d710c47ce7f
Doublo <83957749+Doublo54@users.noreply.github.com>
2025-10-02 16:04:16 +0200

    chore: add test lynex ve strategy

--- src/constants/veStrategies.ts
+++ src/constants/veStrategies.ts
@@ -20,7 +20,15 @@ export const veStrategies: Record<number, VeStrategiesByName> = {
     },
   },
   // Linea (placeholder for future strategies)
-  59144: {},
+  59144: {
+    giga_chad_usdt_stable_maxxer_deluxe_3000_strategy: {
+      managerAddress: "0x421Ba41944a0fdc425d7d6eEd9005c49855882ab",
+      vaultAddress: "0x2ab3379865fF6673CDe7DE4F6bB2Cd12dd542622",
+      rewardToken: "0xA219439258ca9da29E9Cc4cE5596924745e12B93", // USDT
+      enabled: true,
+      maxPools: 5,
+    }
+  },
   // Zircuit (placeholder for future strategies)
   48900: {},
 };
